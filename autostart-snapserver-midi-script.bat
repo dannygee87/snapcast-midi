47e0bc65a775dcfe447f58c73e18bf7677e35174
Daniel Grimm <daniel.grimm87@gmx.de>
2026-08-18 09:08:21 +0200

    new/versions

---
+++ autostart-snapserver-midi-script.bat
@@ -0,0 +1,2 @@
+@echo off
+start "" "C:\Users\danie\Documents\1-programming\Python\snapcast-midi\venv\Scripts\pythonw.exe" "C:\Users\danie\Documents\1-programming\Python\snapcast-midi\snapcast-midi.py"
